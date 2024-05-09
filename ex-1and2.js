d028a1ae54d972a25d9768fe6842ae7d18d2e2b4
Jakkaphob <jkpbeam@gmail.com>
2024-05-09 14:53:26 +0700

    feat: add Javascript values variable as feature/beam

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,17 @@
 //Exercise #1: Variable Creation
 //Start coding here
 
+let name = "Karter";
+let surname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurnatName = "Chef's Table by Chef James";
+const priceBill = 15000;
+const activeRestaurant = 1500;
+
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+priceBill = 5000;
+activeRestaurant = 2500;
