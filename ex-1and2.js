75e8b6305b0a54d6dbc4a4d5dd3121c3979bbbb3
PiPo <pipo.khomgrid@gmail.com>
2024-05-09 13:52:31 +0700

    ex1-4 javascript-value-variable

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstname = "Karter";
+let lastname = "Anderson";
+let addrees = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const resturant = "Chef's Table by Chef James";
+const totalPrice = 15000;
+const openedResturant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+addrees = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalPrice = 5000;
+openedResturant;
--- ex-4.js
+++ ex-4.js
@@ -2,12 +2,19 @@
 50 + 12; // Expression
 
 //Exercise #4: What is Expression Start coding here
-130 + 300;
-let zero = 1000 - 1000;
-20 * 120;
-1000 / 2;
-typeof true;
-let name = "John";
-3 > 5;
-10 == 100;
-200;
+130 + 300; // Yes
+let zero = 1000 - 1000; // No
+20 * 120; // Yes
+1000 / 2; // Yes
+typeof true; // Yes
+let name = "John"; // No
+3 > 5; // Yes
+10 == 100; // Yes
+200; // No
+
+console.log(130 + 300);
+console.log(20 * 120);
+console.log(1000 / 2);
+console.log(typeof true);
+console.log(3 > 5);
+console.log(10 == 100);
